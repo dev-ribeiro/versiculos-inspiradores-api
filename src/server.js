3fc3343a6ceefe9c364f50f9932bbc7800a76aba
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-03-04 11:51:59 -0300

    config: install cors

--- src/server.js
+++ src/server.js
@@ -2,8 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const { router } = require('./routes/index.routes')
+const cors = require('cors')
 const { checkSecretKey } = require('./middlewares/checkSecretKey')
 
+app.use(cors({
+    origin: '*'
+}))
 app.use('/', checkSecretKey, router)
 
 const PORT = process.env.PORT || 3333
