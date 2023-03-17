fefd0e6156be0b51fc4a4c1b157863fa415112e2
David Ribeiro <david.ribeiro.professional@gmail.com>
2023-03-17 13:53:54 -0300

    feat: move pick random verse to Pick Random class

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,9 +1,6 @@
 const express = require('express')
-const fs = require('fs/promises')
-const path = require('path')
 const { checkSecretKey } = require('../middlewares/checkSecretKey')
-const { PickRandomChapter } = require('../services/PickRandomChapter')
-const { pickRandomVerse } = require('../services/pickRandomVerse')
+const { PickRandom } = require('../services/PickRandom')
 
 const router = express.Router()
 
@@ -13,7 +10,7 @@ router.get('/health', (req, res) => {
 
 router.get('/chapters/random', checkSecretKey, async (req, res) => {
     try {
-        const service = new PickRandomChapter()
+        const service = new PickRandom()
         const data = await service.data
         const response = service.getRandomChapter(data)
 
@@ -26,14 +23,11 @@ router.get('/chapters/random', checkSecretKey, async (req, res) => {
 
 router.get('/verses/random', checkSecretKey, async (req, res) => {
     try {
-        const data = await fs.readFile(
-            path.join(__dirname, '../../biblies/nvi.txt'),
-            { encoding: 'utf-8' }
-        )
+        const service = new PickRandom()
+        const data = await service.data
+        const response = service.getRandomVerse(data)
 
-        const verse = pickRandomVerse(JSON.parse(data))
-
-        return res.status(200).json({...verse})
+        return res.status(200).json(response)
     } catch (error) {
         console.log(error)
         return res.status(400).send()
--- src/services/PickRandom/index.js
+++ src/services/PickRandom/index.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises')
 const path = require('path')
 const { getRandomValue } = require("../../utils/getRandomValue")
 
-class PickRandomChapter {
+class PickRandom {
     constructor() {
         this.data = fs.readFile(path.join(__dirname, '../../../biblies/nvi.txt'), { encoding: 'utf-8' })
             .then(response => {
@@ -31,6 +31,30 @@ class PickRandomChapter {
         }
     }
 
+    getRandomVerse(arr){
+        const selectBookIndex = getRandomValue(0, 65)
+        const selectBook = arr
+            .filter((_, index) => {
+                return index === selectBookIndex
+            })
+
+        const chapters = selectBook[0].chapters
+
+        const selectedChapterIndex = getRandomValue(0, chapters.length - 1)
+        const randomVerseIndex = getRandomValue(0, chapters[selectedChapterIndex].length - 1)
+
+        const getVerse = chapters[selectedChapterIndex].filter((_, index) => {
+            return index === randomVerseIndex
+        })[0]
+
+        return {
+            selectedBook: selectBook[0].name,
+            selectedChapterNumber: selectedChapterIndex + 1,
+            selectedVerseNumber: randomVerseIndex + 1,
+            selectedVerse: getVerse
+        }
+    }
+
 }
 
-module.exports = { PickRandomChapter }
+module.exports = { PickRandom }
